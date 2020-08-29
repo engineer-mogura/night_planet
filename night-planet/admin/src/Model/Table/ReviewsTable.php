66230329fcaf6539342aaf5253f6b0a77d348eda
tomori <t.takuma830@gmail.com>
2020-08-29 20:44:13 +0900

    コミ忘れ

---
+++ night-planet/admin/src/Model/Table/ReviewsTable.php
@@ -0,0 +1,112 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Reviews Model
+ *
+ * @property |\Cake\ORM\Association\BelongsTo $Shops
+ * @property |\Cake\ORM\Association\BelongsTo $Users
+ *
+ * @method \App\Model\Entity\Review get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Review newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Review[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Review|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Review saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Review patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Review[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Review findOrCreate($search, callable $callback = null, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class ReviewsTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('reviews');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('shops', [
+            'foreignKey' => 'shop_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsTo('users', [
+            'foreignKey' => 'user_id',
+            'joinType' => 'INNER'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', 'create');
+
+        $validator
+            ->integer('cost')
+            ->requirePresence('cost', 'create')
+            ->allowEmptyString('cost', false);
+
+        $validator
+            ->integer('atmosphere')
+            ->requirePresence('atmosphere', 'create')
+            ->allowEmptyString('atmosphere', false);
+
+        $validator
+            ->integer('customer')
+            ->requirePresence('customer', 'create')
+            ->allowEmptyString('customer', false);
+
+        $validator
+            ->integer('staff')
+            ->requirePresence('staff', 'create')
+            ->allowEmptyString('staff', false);
+
+        $validator
+            ->integer('cleanliness')
+            ->requirePresence('cleanliness', 'create')
+            ->allowEmptyString('cleanliness', false);
+
+        $validator
+            ->scalar('comment')
+            ->allowEmptyString('comment');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['shop_id'], 'shops'));
+        $rules->add($rules->existsIn(['user_id'], 'users'));
+
+        return $rules;
+    }
+}
